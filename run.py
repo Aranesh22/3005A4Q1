e60f1e8a6eb81f96c100b30f2ac1b86ab7be43fa
Aranesh <a.aranesh@gmail.com>
2023-11-23 16:07:07 -0500

    fixed connection with sql plus integration with SQL database fro all inputs

--- run.py
+++ run.py
@@ -4,18 +4,60 @@ hostname = 'localhost'
 database = 'student' 
 username = 'postgres' 
 pwd = 'Vignesh8*' 
-port_id = 5433 
+port_id = 5433  
+conn = None 
+cur = None
 
 try:
     conn = psycopg2.connect ( 
 
-    host = hostname, 
-    dbname = database, 
-    user = username, 
-    password = pwd, 
-    port = port_id)
+        host = hostname, 
+        dbname = database, 
+        user = username, 
+        password = pwd, 
+        port = port_id) 
 
-    conn.close()
+    cur = conn.cursor()
+
+    """
+    script = ''' INSERT INTO students (first_name, last_name, email, enrollment_date) Values (%s,%s,%s,%s)
+                
+    '''   
+    
+    insertVal = ('aran','van','rat@jack.com','2023-09-20')
+    cur.execute(script,insertVal)
+    conn.commit()   
+    """
+
+    """
+    cur.execute('SELECT * FROM STUDENTS') 
+    for x in cur.fetchall(): 
+        print(x)   
+
+    conn.commit()    
+     """  
+    
+    """
+    update_script = 'UPDATE students SET email =%s WHERE student_id = %s' 
+    val = ('a.aranesh@gmail.com',5) 
+    cur.execute(update_script,val) 
+    conn.commit() 
+    """  
+
+
+    delete_script = 'DELETE FROM students WHERE student_id =%s'  
+    val= '5' 
+    cur.execute(delete_script,val) 
+    conn.commit()
+    
 
 except Exception as error: 
-    print(error)
+    print(error) 
+finally:  
+
+    if cur is not None:
+        cur.close() 
+    if conn is not None: 
+        conn.close()
+    cur.close() 
+    conn.close()
